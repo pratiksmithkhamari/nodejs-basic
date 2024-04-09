17deb7d8177d79c9f6031261a237bd16c3ac08ac
pratiksmithkhamari <142924027+pratiksmithkhamari@users.noreply.github.com>
2024-04-10 00:18:57 +0530

    integration of cookies

--- index.js
+++ index.js
@@ -2,10 +2,11 @@
 import express from "express";
 import path from 'path'
 import  mongoose, { Schema }  from "mongoose";
-import { name } from "ejs";
+import cookieParser from "cookie-parser";
+
 const app = express();
 
-const PORT = 4000;
+const PORT = 3000;
 const user= []
 
 mongoose.connect("mongodb://localhost:27017",{
@@ -23,8 +24,59 @@ const Msg = mongoose.model('Message',userSchema)
 app.use(express.urlencoded({extended:true}))
 app.use(express.static(path.join(path.resolve(),'public')))
 
+app.use(cookieParser())
+
 app.get('/public',(req,res)=>{
+
   res.sendFile('public')
+})
+
+const moveToNextUser = (req,res,next)=>{
+  const {token} = req.cookies
+  if(token){
+    res.render('logout.ejs')
+  }else{
+    res.render('login.ejs')
+  }
+}
+
+
+//cookies
+
+app.get('/',(req,res)=>{
+  const {token} = req.cookies
+  if(token){
+    
+    res.render('logout.ejs')
+  }else{
+    res.render('login.ejs')
+  }
+  
+})
+//login so it gets cookie
+app.post('/login',(req,res)=>{
+  res.cookie("token","pratik",{
+    httpOnly:true,
+    expires: new Date(Date.now() + 60 * 1000)
+  })
+  
+  res.redirect('/')
+  
+})
+
+//logout cookie are deleted
+app.get('/logout',(req,res)=>{
+  res.cookie("token",null,{
+    httpOnly:true,
+    expires: new Date(Date.now() )
+  })
+  
+  res.redirect('/')
+  
+})
+
+app.get('/logout',moveToNextUser,(req,res)=>{
+  res.redirect('logout.ejs')
 })
 
 app.get("/", (req, res) => {
@@ -32,7 +84,7 @@ app.get("/", (req, res) => {
 // res.json({name:"pratik"})
 const filePath = path.resolve()
 res.sendFile(path.join(filePath,'./index.html'))
-res.render("index.ejs",{name:"pratik"})
+res.render("index.ejs")
 });
 
 app.get("/add", async (req, res) => {
@@ -42,7 +94,6 @@ app.get("/add", async (req, res) => {
   
 })
   
-
 
 
 app.get("/users", (req, res) => {
@@ -58,12 +109,14 @@ app.get("/success", (req, res) => {
 })
 
 app.post('/',async (req,res)=>{
- 
- await Msg.create({name:req.body.name,email:req.body.email})
+ const {name,email} = req.body
+ await Msg.create({name:name,email:email})
 res.redirect('/success')
 })
 
-
+app.get('/newData',(req,res)=>{
+  res.send('new users')
+})
 
 app.listen(PORT, () => {
   console.log(`this is running on ${PORT}`);
